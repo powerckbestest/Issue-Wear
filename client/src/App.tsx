b6f598356748d9a51c1197a08f662dfad233b26c
Korrusx <o2e3s483@gmail.com>
2023-09-21 09:13:54 +0300

    productSlice

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import MainPage from './components/MainPage';
+
 
 function App(): JSX.Element {
   return (
-    <div className='w-full h-screen text-4xl text-blue-700 flex items-center justify-center'>Tailwind check</div>
+  <Routes> 
+    <Route path='/main' element={<MainPage/>}/>
+
+  </Routes>
   );
 }
 
--- client/src/features/redux/store.ts
+++ client/src/features/redux/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import productReducer from './slices/productSlice'
+
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+      product: productReducer
+    },
   });
   
   export type RootState = ReturnType<typeof store.getState>;
