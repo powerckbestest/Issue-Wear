0d22cdab57d8ac2106346ef9eb750db3f64e0f3a
powerckbestest <ivanchikoff-danila@yandex.ru>
2023-09-22 11:49:57 +0300

    cart done

--- client/src/components/IU/NavBar.tsx
+++ client/src/components/IU/NavBar.tsx
@@ -121,6 +121,16 @@ export default function NavBar() : JSX.Element {
                             <Menu.Item>
                               {({ active }) => (
                                 <a
+                                  href="/cart"
+                                  className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                >
+                                  Shopping cart
+                                </a>
+                              )}
+                            </Menu.Item>
+                            <Menu.Item>
+                              {({ active }) => (
+                                <a
                                   onClick={signOutActionHandler}
                                   className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                 >
--- client/src/components/pages/OneCard.tsx
+++ client/src/components/pages/OneCard.tsx
@@ -1,40 +1,44 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import type { ProductType } from '../../types/productType'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import type { ProductType } from '../../types/productType';
+import useProductHooks from '../../hooks/useProductHooks';
 
 type ProductProps = {
-    product: ProductType
-}
+  product: ProductType;
+};
 
-
-export default function OneCard({product} : ProductProps ) : JSX.Element {
+export default function OneCard({ product }: ProductProps): JSX.Element {
+  const { addProductCartHandler } = useProductHooks();
   return (
-
-     <div>
-            <div key={product.id} className="group relative">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                />
-              </div>
-              <div className="mt-4 flex justify-between">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <NavLink className="nav-link" to={`/products/${product.id}`}>
-                        <span aria-hidden="true" className="absolute inset-0" />
-                        {product.name}
-                    </NavLink>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.color}</p>
-                </div>
-                <p className="text-sm font-medium text-gray-900">{product.price}</p>
-              </div>
-            </div>
-            <button type='button' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            В корзину
-        </button>
-     </div>
-  )
+    <div>
+      <div key={product.id} className="group relative">
+        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+          <img
+            src={product.imageSrc}
+            alt={product.imageAlt}
+            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+          />
+        </div>
+        <div className="mt-4 flex justify-between">
+          <div>
+            <h3 className="text-sm text-gray-700">
+              <NavLink className="nav-link" to={`/products/${product.id}`}>
+                <span aria-hidden="true" className="absolute inset-0" />
+                {product.name}
+              </NavLink>
+            </h3>
+            <p className="mt-1 text-sm text-gray-500">{product.color}</p>
+          </div>
+          <p className="text-sm font-medium text-gray-900">{product.price}</p>
+        </div>
+      </div>
+      <button
+        onClick={(e) => addProductCartHandler(e, product.id)}
+        type="button"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        В корзину
+      </button>
+    </div>
+  );
 }
