e0c3affc5133f81829eb3bf8ed64a245256ba3a0
powerckbestest <ivanchikoff-danila@yandex.ru>
2023-09-27 11:07:00 +0300

    orders

--- client/src/components/pages/AdminOrderPage.tsx
+++ client/src/components/pages/AdminOrderPage.tsx
@@ -51,14 +51,6 @@ export default function AdminOrderPage(): JSX.Element {
             </li>
           ))}
         </ul>
-        <div className="flex justify-center items-center mt-5">
-          <button
-            type="button"
-            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Оформить заказ
-          </button>
-        </div>
       </div>
     </div>
   );
